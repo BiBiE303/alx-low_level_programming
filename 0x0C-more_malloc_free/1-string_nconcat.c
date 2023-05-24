8a817925e184ae9c93a6a7dcd2f6b8202f1ca758
BiBiE303 <bisolababalola16@gmail.com>
2023-05-24 07:59:16 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int l = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int l = 0, k = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
@@ -28,9 +28,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		l++;
 	}
 	while (n < len2 && l < (len1 + n))
-		s[l++] = s2[l++];
+		s[l++] = s2[k++];
 	while (n >= len2 && l < (len1 + len2))
-		s[l++] = s2[l++];
+		s[l++] = s2[k++];
 	s[l] = '\0';
 	return (s);
 }
